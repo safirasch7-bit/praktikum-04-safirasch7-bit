e68203700d4fc9015b6398495bd33185c23f2b4c
Fira <safirasch7gmail.com>
2026-09-24 12:50:16 +0700

    123250084

--- src/student.cpp
+++ src/student.cpp
@@ -224,7 +224,7 @@ static void keadaanRiwayat(DNode* head, DNode* tail) {
     laporkanSalin("riwayatMaju", riwayatMaju(head, penampung), penampung);
     laporkanSalin("riwayatMundur", riwayatMundur(tail, penampung), penampung);
 }
-
+//
 //
 static void keadaanTab(CNode* head) {
     int penampung[KAPASITAS];
